8ffd13af4ec104b5a8cd8e6d4871f9cc0fe451c5
Andrew Plaza <aplaza@liquidthink.net>
2023-06-28 15:29:25 -0400

    fix lazy bootstrap

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.opt.termguicolors = true
 -- bootstrap plugin manager
 require("setup.lazy_boostrap")()
+require("lazy").setup("plugins", require("setup.lazy_opts"))
 require 'utils'
 if isModuleAvailable('impatient') then require('impatient') end
 require('setup/plugin_config')
